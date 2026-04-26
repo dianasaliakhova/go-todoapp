e2c908b63679700391f9839a272df581de60dcda
Diana Saliakhova <diana.saliakhova.work@gmail.com>
2026-04-26 13:54:41 +0100

    feat: infra setup

---
+++ migrations/000001_init.up.sql
@@ -0,0 +1,34 @@
+CREATE SCHEMA todoapp;
+
+-- Создаем в рамках схемы todoapp
+-- PRIMARY первичный ключ таблицы users
+CREATE TABLE todoapp.users (
+    id SERIAL PRIMARY KEY,
+    version BIGINT NOT NULL DEFAULT 1,
+    full_name VARCHAR(100) NOT NULL CHECK (char_length(full_name) BETWEEN 3 AND 100),
+    phone_number VARCHAR(15) CHECK (
+        phone_number ~ '^\+[0-9]+$'
+        AND
+        char_length(phone_number) BETWEEN 10 AND 15
+    )
+);
+
+-- REFERENCES - внешний ключ, который ссылается на таблицу users
+CREATE TABLE todoapp.tasks (
+    id SERIAL PRIMARY KEY,
+    version BIGINT NOT NULL DEFAULT 1,
+    title VARCHAR(100) NOT NULL CHECK (char_length(title) BETWEEN 1 AND 100),
+    description VARCHAR(1000) CHECK (char_length(description) BETWEEN 1 AND 1000),
+    completed BOOLEAN NOT NULL,
+    created_at TIMESTAMPTZ NOT NULL,
+    completed_at TIMESTAMPTZ,
+
+    -- тк проверка зависит не от одного поля, пишем тут check глобально. вызывается если есть поля которые чекаются
+    CHECK (
+        (completed=FALSE AND completed_at IS NULL)
+        OR
+        (completed=TRUE AND completed_at IS NOT NULL AND completed_at >= created_at)
+    ),
+    
+    author_user_id INTEGER NOT NULL REFERENCES todoapp.users(id)
+);
